1ff45c153f8640a49419291ebfd6ab8b1e07e0d4
mzenteno.intedes@gmail.com <mzenteno.intedes@gmail.com>
2024-10-12 01:08:46 -0400

    configuracion de cors en SecurityConfig

--- src/main/java/com/mzenteno/clinica/config/CorsConfig.java
+++ src/main/java/com/mzenteno/clinica/config/CorsConfig.java
@@ -1,27 +1,26 @@
-package com.mzenteno.clinica.config;
+// package com.mzenteno.clinica.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.CorsConfigurationSource;
+// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:5173"); // Permitir origen de React
-        configuration.addAllowedOrigin("https://lucky-baklava-607e71.netlify.app"); // Permitir origen de React
-        configuration.addAllowedOrigin("https://web-clinica.onrender.com"); // Permitir origen de React
-        configuration.addAllowedHeader("*"); // Permitir todos los encabezados
-        configuration.addAllowedMethod("*"); // Permitir todos los métodos HTTP
-        configuration.setAllowCredentials(true); // Permitir credenciales
+//     @Bean
+//     public CorsConfigurationSource corsConfigurationSource() {
+//         CorsConfiguration configuration = new CorsConfiguration();
+//         configuration.addAllowedOrigin("http://localhost:5173"); // Permitir origen de React
+//         configuration.addAllowedOrigin("https://web-clinica.onrender.com"); // Permitir origen de React
+//         configuration.addAllowedHeader("*"); // Permitir todos los encabezados
+//         configuration.addAllowedMethod("*"); // Permitir todos los métodos HTTP
+//         configuration.setAllowCredentials(true); // Permitir credenciales
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // Aplicar CORS a todas las rutas
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         source.registerCorsConfiguration("/**", configuration); // Aplicar CORS a todas las rutas
 
-        return source; // Devolver el source como CorsConfigurationSource
-    }
-}
+//         return source; // Devolver el source como CorsConfigurationSource
+//     }
+// }
--- src/main/java/com/mzenteno/clinica/config/SecurityConfig.java
+++ src/main/java/com/mzenteno/clinica/config/SecurityConfig.java
@@ -13,20 +13,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-/*   @Autowired
-  private UserDetailsServiceImpl userDetailsService; */
-
   @Autowired
   SecurityFilter securityFilter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {    
     http
+      .cors(cors -> cors.configurationSource(corsConfigurationSource()))  // Configuración de CORS
       .csrf(csrf -> csrf.disable())
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authorizeHttpRequests(authorize -> authorize
@@ -37,13 +37,6 @@ public class SecurityConfig {
         .anyRequest().authenticated()
       )
       .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
-
-    // http
-    // .csrf(csrf -> csrf.disable())
-    //         .authorizeHttpRequests(authorize -> authorize
-    //             .anyRequest().permitAll() // Permitir todas las solicitudes
-    //         );
-
     return http.build();
   }
 
@@ -55,6 +48,20 @@ public class SecurityConfig {
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
     return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.addAllowedOrigin("http://localhost:5173"); // Origen local
+    configuration.addAllowedOrigin("https://web-clinica.onrender.com"); // Origen en Render
+    configuration.addAllowedHeader("*"); // Permitir todos los encabezados
+    configuration.addAllowedMethod("*"); // Permitir todos los métodos (GET, POST, PUT, etc.)
+    configuration.setAllowCredentials(true); // Permitir credenciales
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration); // Aplicar CORS a todas las rutas
+    return source;
   }
   
 }
